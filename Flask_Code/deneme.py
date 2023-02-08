6676b63cde0f4d5f62337146941c0547b30d9187
Melih Selami Urkmez (Dogus Teknoloji) <melih.urkmez@d-teknoloji.com.tr>
2023-02-08 16:26:32 +0300

    Add v3 docker image

--- Flask_Code/deneme.py
+++ Flask_Code/deneme.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This container version v2.'
+    return 'Gul artik Djangoya baslasin.! -Msuwashere'
 
 if __name__== '__main__':
     app.run(debug=True, host='0.0.0.0')
