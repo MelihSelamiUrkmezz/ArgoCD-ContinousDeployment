1554e3e5c8c12ba8efc0a8c36a6e83aa3ff5ff97
Melih Selami Urkmez (Dogus Teknoloji) <melih.urkmez@d-teknoloji.com.tr>
2023-02-09 13:20:50 +0300

    Add changes

--- Flask_Code/deneme.py
+++ Flask_Code/deneme.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Gul artik Djangoya baslasin.! -Msuwashere'
+    return 'This container version is v2.'
 
 if __name__== '__main__':
     app.run(debug=True, host='0.0.0.0')
